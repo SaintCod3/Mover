175e2e80be49e2a8de285d924d4b37abcd2b9763
Saint <39530610+SaintCod3@users.noreply.github.com>
2023-11-05 23:06:20 +0000

    Add files via upload

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.nombres = ["Consola 1", "Consola 2", "Consola 3", "Consola 4", "Consola 5"]
+        self.nombres = ["PSX", "PS2", "Dreamcast", "Genesis", "PC-FX"]
         self.directorios = {}
 
         self.layout = QVBoxLayout()
