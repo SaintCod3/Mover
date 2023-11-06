51388d38d944360a12549c5a73dc473ef98f0606
Saint <39530610+SaintCod3@users.noreply.github.com>
2023-11-06 08:14:05 +0000

    POC
    POC of opening the configuration for directories and a simple browser for a directory that can be chosen by the user

---
+++ browser.py
@@ -0,0 +1,43 @@
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QFileDialog, QListWidget, QPushButton
+from PyQt5.QtCore import QProcess
+
+class FileExplorer(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.list_widget = QListWidget()
+        self.layout.addWidget(self.list_widget)
+
+        open_button = QPushButton('Seleccionar directorio')
+        open_button.clicked.connect(self.open_directory)
+        self.layout.addWidget(open_button)
+
+        config_button = QPushButton('Configuración')
+        config_button.clicked.connect(self.open_configuration_script)
+        self.layout.addWidget(config_button)
+
+    def open_directory(self):
+        directory = QFileDialog.getExistingDirectory(self, 'Seleccionar directorio')
+        if directory:
+            self.list_directory_contents(directory)
+
+    def list_directory_contents(self, directory):
+        self.list_widget.clear()
+        for item in os.listdir(directory):
+            self.list_widget.addItem(item)
+
+    def open_configuration_script(self):
+        script_path = "config.py" 
+        if os.path.exists(script_path):
+            QProcess.startDetached('python', [script_path])
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = FileExplorer()
+    window.show()
+    sys.exit(app.exec_())
